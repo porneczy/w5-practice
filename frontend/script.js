48aa97e5d3e7456183b874c7d1d47c00b96ead7e
porneczy <porneczy@gmail.com>
2022-02-16 14:44:25 +0100

    monday

---
+++ frontend/script.js
@@ -0,0 +1,111 @@
+// console.log(a)
+// console.log(c)
+
+// console.log("Hello Word!");
+// console.log('Hello Word!');
+// console.log(`Hello
+//      Word!`);
+// console.log(typeof 2);
+// console.log(typeof NaN);
+// console.log(0/0);
+// console.log(2.14);
+// console.log(true)
+// console.log(false)
+// console.log(undefined)
+// console.log(null)
+// console.log(typeof undefined)
+// console.log(typeof null)
+/* 
+console.log({
+    "title": "Start Coding",
+    "type": "Solo",
+    "difficulty": 4.1,
+    "usefulness": 4.2,
+    "enjoyability": 3.7,
+    "materials": 3.4,
+    "tasks": [
+        {
+            title: "Bank accounts",
+            description:""
+        },
+        {
+            title: "100 doors",
+            description:""
+        },
+        {
+            title: "Word filter",
+            description:""
+        }
+    ]
+}) */
+/* 
+
+var a = "asd"
+let b = "asd2"
+const c = "asd3"
+
+console.log(a)
+console.log(b)
+console.log(c)
+ */
+
+
+/* function d (){
+    if(1 + 1 === 2){
+        
+    }
+    var a = "asd"
+    let b = "asd2"
+    const c = "asd3"
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+d() */
+
+/* let e = 1
+console.log(e)
+e = 2
+console.log(e)
+e = e * 2
+console.log(e) */
+
+
+/* function multiplyByTwo(num){
+    console.log(num)
+    return num*2
+}
+let f = multiplyByTwo(1)
+console.log(f)
+console.log( multiplyByTwo(10) )
+console.log(num)
+ */
+/* let g = {
+    myMethod: function(){
+        console.log("hello")
+    }
+}
+g.myMethod()
+ */
+
+//                              ez nem fog működni csak megnéztük hogy működik egy fg a motorháztető alatt, példánkon az window addeyent
+/* let window = {
+    addEventListener: function (eventName, eventFunction){
+        if( eventName === "load" ){
+            eventFunction()
+        }
+    }
+}
+window.addEventListener("load", loadEvent) */
+
+
+let h = {
+    anotherMethod: function (text){
+        return `You sent the following argument to this method: ${text}`
+    }
+}
+
+let i = h.anotherMethod("sdasdaasd")
+console.log(i)
+console.log(h.anotherMethod("kurva easy"));
+
